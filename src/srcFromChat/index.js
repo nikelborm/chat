13098dcd08635eb019a4e3460b80127595693a28
nikelborm <r0075301net.r@yandex.ru>
2020-07-04 17:42:36 +0300

    Слив всей кодовой базы в одну

---
+++ src/srcFromChat/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import {render} from "react-dom";
+import App from "./components/App.js";
+
+render(
+    <React.StrictMode><App /></React.StrictMode>,
+    document.getElementById("root")
+);
