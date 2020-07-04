65a756d19cea09417fde9c3d977a850526028007
nikelborm <r0075301net.r@yandex.ru>
2020-07-04 17:45:13 +0300

    фыва

--- functionsChat/clearCookies.js
+++
@@ -1,7 +0,0 @@
-function clearCookies(response, ...params) {
-    // * Удаляет все куки с именами из params
-    for (const param of params) {
-        response.clearCookie(param);
-    }
-}
-exports.clearCookies = clearCookies;
--- functionsChat/isAllStrings.js
+++
@@ -1,9 +0,0 @@
-function isAllStrings(body) {
-    // * Проверяет все ли значения в body являются строками
-    let result = 1;
-    for (let key in body) {
-        result &= +(typeof body[key] === "string");
-    }
-    return result;
-}
-exports.isAllStrings = isAllStrings;
