53468d3101d2467ec2d5e76d1a6d47e23a6bfe4a
nikelborm <r0075301net.r@yandex.ru>
2020-07-04 17:33:35 +0300

    blabla

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import {render} from "react-dom";
-import App from "./components/App.js";
-
-render(
-    <React.StrictMode><App /></React.StrictMode>,
-    document.getElementById("root")
-);
